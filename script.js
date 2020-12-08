2a79b6ec48908a9b80b74929b460803fbd8f4f65
Yann8313 <yannick.ayassa@gmail.com>
2020-12-07 19:57:34 -0500

    update

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-/*****VARIABLES DOM ******/
 
+/*Adress */ 
+const adressList = document.getElementById("adressList");
+const adress = document.getElementById('adress')
+
+
+const updateValueAdress = (e) => {
+  adress.textContent = e.target.value;
+};
+
+adressList.addEventListener("change", updateValueAdress);
+
+
+/* Name */ 
+const inputName = document.getElementById("name");
+const userName = document.getElementById("userName");
+
+const updateValueName = (e) => {
+  userName.textContent = e.target.value;
+};
+
+inputName.addEventListener("input", updateValueName);
